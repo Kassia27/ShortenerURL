16ca90385a327604af55fde9fa0673ae7cc09fd7
Rita <113695803+Kassia27@users.noreply.github.com>
2023-11-03 15:03:37 -0300

    Update redutorURL.php

---
+++ redutorURL.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redutor URL</title>
+</head>
+<body>
+    <form action="">
+        <input type="text">
+        <button>Reduzir</button>
+    </form>
+</body>
+</html>
