32b9c405f17f9c7b5b61ba1d9bf736cba2145371
Rita <113695803+Kassia27@users.noreply.github.com>
2023-11-03 16:48:12 -0300

    Update redutorURL.php

--- redutorURL.php
+++ redutorURL.php
@@ -6,9 +6,10 @@
     <title>Redutor URL</title>
 </head>
 <body>
+    <h2>Redutor de URL</h2>
     <form action="">
-        <input type="text">
-        <button>Reduzir</button>
+        <input type="text" type="text" name="url">
+        <button type="submit" name="buttom">Reduzir</button>
     </form>
 </body>
 </html>
